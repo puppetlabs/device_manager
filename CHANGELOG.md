b66e5a37ce0e19507997e94cb9a6294102cecaa9
tkishel <tkishel@gmail.com>
2017-08-29 14:59:17 -0700

    hiera and each

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,6 +1,18 @@
+## Release 1.1.0
+
+### Summary
+
+Minor changes.
+
+#### Changed
+
+- Use hiera_hash to improve compatibility.
+- Use iteration instead of create_resource to improve readability.
+
 ## Release 1.0.9
 
 ### Summary
+
 This release allows users to declare devices as a hash of hashes in Hiera.
 
 #### Features/Improvements
--- manifests/devices.pp
+++ manifests/devices.pp
@@ -1,6 +1,17 @@
 # Read all devices from Hiera.
 
 class puppet_device::devices {
-  $devices = lookup('puppet_device::devices', Hash, 'hash', {})
-  create_resources(puppet_device, $devices)
+# $devices = lookup('puppet_device::devices', Hash, 'hash', {})
+  $devices = hiera_hash('puppet_device::devices', {})
+
+# create_resources(puppet_device, $devices)
+  $devices.each |$title, $device| {
+    puppet_device {$title:
+      name  => $device['name'],
+      type  => $device['type'],
+      url   => $device['url'],
+      debug => $device['debug'],
+      run   => $device['run'],
+     }
+  }
 }
