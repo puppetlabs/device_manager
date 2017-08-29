b825f6b661c1e2b5a04722c77c6bae4155db88c1
tkishel <tkishel@gmail.com>
2017-08-29 16:08:10 -0700

    avoid hiera warning

--- manifests/devices.pp
+++ manifests/devices.pp
@@ -1,10 +1,18 @@
 # Read all devices from Hiera.
 
 class puppet_device::devices {
-# $devices = lookup('puppet_device::devices', Hash, 'hash', {})
-  $devices = hiera_hash('puppet_device::devices', {})
 
-# create_resources(puppet_device, $devices)
+  # Avoid the Hiera 5 'hiera_hash is deprecated' warning.
+
+  if ( (versioncmp($::clientversion, '4.9.0') >= 0) and (! defined('$::serverversion') or versioncmp($::serverversion, '4.9.0') >= 0) ) {
+    $devices = lookup('puppet_device::devices', Hash, 'hash', {})
+  } else {
+    $devices = hiera_hash('puppet_device::devices', {})
+  }
+
+  # Use $devices.each ... or create_resources(puppet_device, $devices)?
+  # Consider: http://garylarizza.com/blog/2014/10/24/puppet-workflows-4-using-hiera-in-anger/
+
   $devices.each |$title, $device| {
     puppet_device {$title:
       name  => $device['name'],
