08500ba671058eb382e81a72ab144a5a9d0d7e9a
tkishel <tkishel@gmail.com>
2018-02-01 13:43:22 -0800

    sanitize

--- tasks/init.rb
+++ tasks/init.rb
@@ -8,14 +8,19 @@ require 'timeout'
 
 ####
 
+default_timeout = 64
 params = JSON.parse(STDIN.read)
 noop = (params['_noop']) ? '--noop' : ''
 target = (params['target']) ? params['target'] : ''
-timeout = (params['timeout'].to_i > 0) ? params['timeout'].to_i : 4
-puppet = (%r{mingw} =~ RUBY_PLATFORM) ? '"C:\Program Files\Puppet Labs\Puppet\bin\puppet"' : '/opt/puppetlabs/puppet/bin/puppet'
-command = "#{puppet} device --user=root -v --waitforcert=0 #{noop}"
+timeout = (params['timeout'].to_i > 0) ? params['timeout'].to_i : default_timeout
 
 ####
+
+# def safe_target(target)
+#  (target =~ %r{^[a-z0-9._-]+$}) == 0
+# end
+
+#
 
 def read_device_config(target)
   Puppet.initialize_settings
@@ -28,11 +33,13 @@ end
 
 #
 
-def run_puppet_device(command, timeout, devices)
+def run_puppet_device(devices, noop, timeout)
+  puppet = (%r{mingw} =~ RUBY_PLATFORM) ? '"C:\Program Files\Puppet Labs\Puppet\bin\puppet"' : '/opt/puppetlabs/puppet/bin/puppet'
   results = {}
   results['error_count'] = 0
 
   devices.collect do |device_name, _device|
+    target = "--target=#{device_name}"
     line = ''
     error_message = ''
     configuration_version = ''
@@ -41,13 +48,16 @@ def run_puppet_device(command, timeout, devices)
     result = ''
 
     begin
-      Open3.popen2e("#{command} --target #{device_name}") do |_, oe, w|
+      Open3.popen2e(puppet, 'device', '--user=root', '--waitforcert=0', '-v', target, noop) do |_, oe, w|
         begin
           Timeout.timeout(timeout) do
             until oe.eof?
               line = oe.readline
               if (matched = line.match(%r{Error: (?<error>.*)}))
                 error_message = matched[:error]
+              end
+              if (matched = line.match(%r{Exiting; (?<certificate>no certificate found and waitforcert is disabled)}))
+                error_message = matched[:certificate]
               end
               if (matched = line.match(%r{Applying configuration version '(?<version>.*?)'}))
                 configuration_version = matched[:version]
@@ -68,13 +78,13 @@ def run_puppet_device(command, timeout, devices)
 
     error_message.gsub!(%r{\e\[(\d+)m}, '')
 
-    if configuration_version != '' && catalog_seconds != '' && error_message == ''
+    if error_message == ''
       status = 'success'
       result = "applied configuration version '#{configuration_version}' in #{catalog_seconds} seconds"
     else
       status = 'error'
       if error_message == ''
-        error_message = 'unable to parse the output of the puppet device command'
+        error_message = "unable to parse the output of the puppet device command: #{error_message}"
       end
       result = error_message
       results['error_count'] = results['error_count'] + 1
@@ -117,7 +127,7 @@ def return_result(params, results)
     exit_code = 1
     plurality = (results['error_count'] > 1) ? 's' : ''
     result[:_error] = {
-      msg: "puppet device run error#{plurality}: review status with `puppet job show`",
+      msg: "puppet device run error#{plurality}: review status via the Console",
       kind: 'tkishel/puppet_device',
       details: {
         params: {
@@ -139,10 +149,9 @@ end
 
 ####
 
-configured_devices = read_device_config(target)
-
-if configured_devices.count > 0
-  results = run_puppet_device(command, timeout, configured_devices)
+devices = read_device_config(target)
+if devices.count > 0
+  results = run_puppet_device(devices, noop, timeout)
   return_result(params, results)
 else
   return_result_read_device_config_error(params)
