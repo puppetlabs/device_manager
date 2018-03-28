bc1984e02a52069e31aaeb6f4a8bea463163dc93
tkishel <tkishel@gmail.com>
2018-03-28 12:05:55 -0700

    add run_interval

--- examples/init.pp
+++ examples/init.pp
@@ -1,6 +1,7 @@
 # Smoke test.
 
 puppet_device {'bigip.example.com':
-  type => 'f5',
-  url  => 'https://admin:fffff55555@10.0.0.245/',
+  type         => 'f5',
+  url          => 'https://admin:fffff55555@10.0.0.245/',
+  run_interval => 30,
 }
