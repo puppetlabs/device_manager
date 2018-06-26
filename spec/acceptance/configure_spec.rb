5c95661754e78f16b569380b458c3f1214d52a65
tkishel <tkishel@gmail.com>
2018-06-26 12:39:12 -0700

    (FM-7115) return device certficate with task results
    Add puppet device and puppt task tests.

--- spec/acceptance/configure_spec.rb
+++ spec/acceptance/configure_spec.rb
@@ -1,16 +1,28 @@
 require 'spec_helper_acceptance'
+
 describe 'configure' do
   context 'basic setup' do
     it 'edit site.pp and run the agent' do
       fqdn = fact('fqdn')
       pp = <<-EOS
-      node '#{fqdn}' {
-        device_manager {'bigip.example.com':
-          type         => 'f5',
-          url          => 'https://admin:fffff55555@10.0.0.245/',
-          run_interval => 30,
-        }
-      }
+node '#{fqdn}' {
+  device_manager {'cisco.example.com':
+    type        => 'cisco_ios',
+    credentials => {
+	  address         => '10.64.21.10',
+	  port            => 22,
+	  username        => 'root',
+	  password        => 'eq3e2jM6m8AVvT9',
+	  enable_password => 'eq3e2jM6m8AVvT9',
+    },
+  }
+  device_manager {'bigip.example.com':
+    type         => 'f5',
+    url          => 'https://admin:fffff55555@10.0.0.245/',
+    run_interval => 30,
+  }
+}
+node default {}
       EOS
       make_site_pp(pp)
       run_agent(allow_changes: true)
@@ -20,19 +32,40 @@ describe 'configure' do
     # check device.conf is created
     describe file('/etc/puppetlabs/puppet/device.conf') do
       it { is_expected.to be_file }
+      it { is_expected.to contain %r{[cisco.example.com]} }
+      it { is_expected.to contain %r{type cisco_ios} }
       it { is_expected.to contain %r{[bigip.example.com]} }
       it { is_expected.to contain %r{type f5} }
     end
+  end
+end
 
-    # check crontab has an entry
-    it 'crontab entry' do
-      result = on(default, 'crontab -l').stdout
-      expect(result).to match(%r{puppet device})
+describe 'run' do
+  context 'puppet device' do
+    it 'generate and sign a certificate request' do
+      run_cert_reset('cisco.example.com')
+      run_device_generate_csr('cisco.example.com')
+      run_cert_sign('cisco.example.com')
     end
+    it 'runs puppet device' do
+      run_device('cisco.example.com', allow_changes: false)
+    end
+  end
 
-    it 'run puppet device' do
-      # check the cert is created
-      # sign the cert
+  context 'puppet device tasks' do
+    it 'puppet task run' do
+      # PE vs FOSS
+      ENV['PUPPET_INSTALL_TYPE'] = 'pe'
+      run_puppet_access_login(user: 'admin')
+      proxy_cert_name = fact('fqdn')
+      device_cert_name = 'cisco.example.com'
+      # TODO: Read the default certificate fingerprint and add to regex below.
+      run_and_expect(proxy_cert_name, device_cert_name,
+        [
+          %r{status : success},
+          %r{fingerprint :},
+        ]
+      )
     end
   end
 end
--- spec/spec_helper_acceptance.rb
+++ spec/spec_helper_acceptance.rb
@@ -1,5 +1,6 @@
 require 'beaker-rspec/spec_helper'
 require 'beaker-rspec/helpers/serverspec'
+require 'beaker-task_helper'
 require 'beaker/puppet_install_helper'
 require 'beaker/module_install_helper'
 require 'pry'
@@ -20,13 +21,34 @@ def make_site_pp(pp)
   wait_for_master(3)
 end
 
-def run_device(options = { allow_changes: true })
-  acceptable_exit_codes = if options[:allow_changes] == false
-                            0
-                          else
-                            [0, 2]
-                          end
-  on(default, puppet('device', '--verbose', '--trace'), acceptable_exit_codes: acceptable_exit_codes) do |result|
+def run_agent(options = { allow_changes: true })
+  acceptable_exit_codes = (options[:allow_changes] == false) ? 0 : [0, 2]
+  on(default, puppet('agent', '-t'),  acceptable_exit_codes: acceptable_exit_codes)
+end
+
+def run_device_generate_csr(cert_name)
+  acceptable_exit_codes = 1
+  on(default, puppet('device', '--verbose', '--trace', '--waitforcert=0', '--target', cert_name), acceptable_exit_codes: acceptable_exit_codes) do |result|
+    expect(result.stdout).to match(%r{Exiting; no certificate found and waitforcert is disabled})
+  end
+end
+
+def run_cert_reset(cert_name)
+  on master, "puppet node purge #{cert_name}"
+  on default, "rm -rf /opt/puppetlabs/puppet/cache/devices/#{cert_name}"
+end
+
+def run_cert_sign(cert_name = nil)
+  if cert_name
+    on(master, puppet('cert', 'sign', '--debug', cert_name), acceptable_exit_codes: [0, 1]).stdout
+  else
+    on(master, puppet('cert', 'sign', '--all', '--debug'), acceptable_exit_codes: [0, 1]).stdout
+  end
+end
+
+def run_device(cert_name, options = { allow_changes: true })
+  acceptable_exit_codes = (options[:allow_changes] == false) ? 0 : [0, 2]
+  on(default, puppet('device', '--verbose', '--trace', '--waitforcert=0', '--target', cert_name), acceptable_exit_codes: acceptable_exit_codes) do |result|
     # on(default, puppet('device','--verbose','--color','false','--user','root','--trace','--server',master.to_s), { :acceptable_exit_codes => acceptable_exit_codes }) do |result|
     if options[:allow_changes] == false
       expect(result.stdout).not_to match(%r{^Notice: /Stage\[main\]})
@@ -36,27 +58,24 @@ def run_device(options = { allow_changes: true })
   end
 end
 
-def run_resource(resource_type, resource_title = nil)
-  if resource_title
-    on(master, puppet('device', '--target', 'target', '--resource', resource_type, resource_title, '--trace'), acceptable_exit_codes: [0, 1]).stdout
-  else
-    on(master, puppet('device', '--target', 'target', '--resource', resource_type, '--trace'), acceptable_exit_codes: [0, 1]).stdout
-  end
+def run_and_expect(proxy_cert_name, device_cert_name, regexes)
+  expect_multiple_regexes(result: run_task(task_name: 'device_manager::run_puppet_device', host: proxy_cert_name, params: "target=#{device_cert_name}"), regexes: regexes)
 end
 
-def run_agent(options = { allow_changes: true })
-  acceptable_exit_codes = if options[:allow_changes] == false
-                            0
-                          else
-                            [0, 2]
-                          end
-  on(default, puppet('agent', '-t'),  acceptable_exit_codes: acceptable_exit_codes)
+
+def run_resource(cert_name, resource_type, resource_title = nil)
+  if resource_title
+    on(master, puppet('device', '--target', cert_name, '--resource', resource_type, resource_title, '--trace'), acceptable_exit_codes: [0, 1]).stdout
+  else
+    on(master, puppet('device', '--target', cert_name, '--resource', resource_type, '--trace'), acceptable_exit_codes: [0, 1]).stdout
+  end
 end
 
 RSpec.configure do |c|
   c.before :suite do
     unless ENV['BEAKER_TESTMODE'] == 'local'
       unless ENV['BEAKER_provision'] == 'no'
+        install_module_from_forge('puppetlabs-cisco_ios', '0.2.0')
         install_module_from_forge('f5-f5', '1.8.0')
       end
       hosts.each do |host|
