e482e818346337db255bca06c781cfc7f2bba6ee
tkishel <tkishel@gmail.com>
2018-08-14 11:37:38 -0700

    (FM-7230) Specify defaults for multiple devices using Hiera or Classifier
    This commit implements global and device-type levels of default values.
    These defaults are used by:
      device_manager defined type
      device_manager::devices class

--- manifests/devices.pp
+++ manifests/devices.pp
@@ -1,41 +1,18 @@
 # device_manager::devices
 #
-# Configure multiple devices through data.
-# Use either the `$devices` parameter to pass in data through the Classifier,
-# or set the `device_manager::devices` key in Hiera.
-# If configuration for the same device is specified in both,
-# the `$devices` parameter wins.
+# Configure multiple devices and device defaults via data.
 #
-# @summary Configure multiple devices through data.
+# Use either keys in Hiera, or parameters in the Classifier.
+# If the same configuration is specified in both, the Classifier wins.
 #
-# @example in Hiera:
-#   ---
-#   device_manager::devices:
-#     bigip1.example.com:
-#       type:         'f5'
-#       url:          'https://admin:fffff55555@10.0.1.245/'
-#       run_interval: 30
-#     bigip2.example.com:
-#       type:         'f5'
-#       url:          'https://admin:fffff55555@10.0.2.245/'
-#       run_interval: 30
+# See examples/devices.yaml
 #
-# @example The datastructure to pass through the Classifier is exactly the same:
-#   class { 'device_manager::devices':
-#     devices => {
-#       'bigip1.example.com' => {
-#         type => 'f5',
-#         url  => 'https://admin:fffff55555@10.0.1.245/',
-#         run_interval => 30,
-#       },
-#       'bigip2.example.com' => {
-#         type => 'f5',
-#         url  => 'https://admin:fffff55555@10.0.2.245/',
-#         run_interval => 30,
-#       },
-#     }
-#   }
-class device_manager::devices(Hash $devices = {}) {
+# @summary Configure multiple devices and device defaults via data.
+
+class device_manager::devices (
+  Hash $devices  = {},
+  Hash $defaults = {},
+) {
 
   # Validate node.
 
@@ -43,25 +20,66 @@ class device_manager::devices(Hash $devices = {}) {
     fail("Classification Error: 'device_manager::devices' declared on a device instead of an agent.")
   }
 
+  # Initialize the concat resources used by conf and fact.
+  # This allows an empty device_manager::devices hash to clear undeclared devices from conf and facts.
+  # Note that concat is not used by run, resulting in orphaned Cron (or Scheduled Task) resources.
+  # Best practice is to ensure => absent via device defaults instead of an empty device_manager::devices hash.
+
+  include device_manager::conf
+  include device_manager::fact
+
   # Avoid the Hiera 5 'hiera_hash is deprecated' warning.
 
   if ( (versioncmp($::clientversion, '4.9.0') >= 0) and (! defined('$::serverversion') or versioncmp($::serverversion, '4.9.0') >= 0) ) {
-    $hiera_devices = lookup('device_manager::devices', Hash, 'hash', {})
+    $hiera_devices  = lookup('device_manager::devices', Hash, 'hash', {})
+    $hiera_defaults = lookup('device_manager::defaults', Hash, 'hash', {})
   } else {
-    $hiera_devices = hiera_hash('device_manager::devices', {})
+    $hiera_devices  = hiera_hash('device_manager::devices', {})
+    $hiera_defaults = hiera_hash('device_manager::defaults', {})
   }
 
-  ($hiera_devices + $devices).each |$title, $device| {
-    device_manager {$title:
-      name           => $device['name'],
-      type           => $device['type'],
-      url            => $device['url'],
-      credentials    => $device['credentials'],
-      debug          => $device['debug'],
-      run_interval   => $device['run_interval'],
-      run_via_exec   => $device['run_via_exec'],
-      include_module => $device['include_module'],
+  # Combine data from Hiera and the Classifier.
+
+  $_devices = $hiera_devices + $devices
+  $_defaults = deep_merge($hiera_defaults, $defaults)
+
+  $_devices.each |$title, $_device| {
+
+    # Note: type cannot be a bare word hash key name.
+    $key = 'type'
+
+    if ($_device[$key] != undef) {
+      $_type = $_device[$key]
+    } else {
+      $_type = $_defaults[$key]
+    }
+
+    if ($_type == undef) {
+      fail('Parameter Error: type is required')
+    }
+
+    # Use the type to identify type-level defaults.
+
+    if has_key($_defaults, $_type) {
+      $_device_type_defaults = $_defaults[$_type]
+    } else {
+      $_device_type_defaults = {}
+    }
+
+    # Merge defaults and parameters. Note: the rightmost hash takes precedence.
+
+    $params = deep_merge($_defaults, $_device_type_defaults, $_device)
+
+    device_manager { $title:
+      ensure         => $params['ensure'],
+      name           => $params['name'],
+      type           => $params['type'],
+      url            => $params['url'],
+      credentials    => $params['credentials'],
+      debug          => $params['debug'],
+      run_interval   => $params['run_interval'],
+      run_via_exec   => $params['run_via_exec'],
+      include_module => $params['include_module'],
     }
   }
-
 }
--- spec/acceptance/configure_spec.rb
+++ spec/acceptance/configure_spec.rb
@@ -1,57 +1,150 @@
 require 'spec_helper_acceptance'
 
 describe 'configure' do
-  context 'device management' do
-    it 'define device management in site.pp on the master' do
-      fqdn = fact('fqdn')
+  fqdn = fact('fqdn')
 
-      manifest = <<-EOS
-  node '#{fqdn}' {
-    device_manager {'bigip.example.com':
-      type         => 'f5',
-      url          => 'https://admin:fffff55555@10.0.0.245/',
-      run_interval => 30,
-    }
-
-    device_manager {'cisco.example.com':
-      type        => 'cisco_ios',
-      credentials => {
-        address         => '10.64.21.10',
-        port            => 22,
-        username        => 'root',
-        password        => 'eq3e2jM6m8AVvT9',
-        enable_password => 'eq3e2jM6m8AVvT9',
-      },
-    }
+  manifest = <<-EOS
+File { backup => false }
+node '#{fqdn}' {
+  device_manager {'bigip.example.com':
+    type         => 'f5',
+    url          => 'https://admin:password@10.0.0.245/',
+    run_interval => 30,
   }
-  node default {}
-      EOS
+  device_manager {'cisco.example.com':
+    type        => 'cisco_ios',
+    credentials => {
+      address         => '10.64.21.10',
+      port            => 22,
+      username        => 'root',
+      password        => 'eq3e2jM6m8AVvT9',
+      enable_password => 'eq3e2jM6m8AVvT9',
+    },
+  }
+}
+node default {}
+EOS
 
+  manifest_with_include_devices = <<-EOS
+File { backup => false }
+node '#{fqdn}' {
+  device_manager {'bigip.example.com':
+    type         => 'f5',
+    url          => 'https://admin:password@10.0.0.245/',
+    run_interval => 30,
+  }
+  device_manager {'cisco.example.com':
+    type        => 'cisco_ios',
+    credentials => {
+      address         => '10.64.21.10',
+      port            => 22,
+      username        => 'root',
+      password        => 'eq3e2jM6m8AVvT9',
+      enable_password => 'eq3e2jM6m8AVvT9',
+    },
+  }
+  include device_manager::devices
+}
+node default {}
+EOS
+
+  yaml_with_devices = <<-EOS
+---
+device_manager::devices:
+  cisco_via_hiera.example.com:
+    type:         'cisco_ios'
+    credentials:
+      address:         '10.64.21.10'
+      port:            22
+      username:        'admin'
+      password:        'eq3e2jM6m8AVvT9'
+      enable_password: 'eq3e2jM6m8AVvT9'
+device_manager::defaults:
+  run_interval: 60
+EOS
+
+  yaml_with_devices_with_ensure_absent = <<-EOS
+---
+device_manager::devices:
+  cisco_via_hiera.example.com:
+    type:         'cisco_ios'
+    credentials:
+      address:         '10.64.21.10'
+      port:            22
+      username:        'admin'
+      password:        'eq3e2jM6m8AVvT9'
+      enable_password: 'eq3e2jM6m8AVvT9'
+device_manager::defaults:
+  ensure:       'absent'
+  run_interval: 60
+EOS
+
+  context 'device management via device_manager' do
+    it 'define device management in site.pp on the master' do
       define_site_pp(manifest)
     end
-
     it 'define device management on the proxy agent' do
       run_puppet_agent(allow_changes: true)
       run_puppet_agent(allow_changes: false)
     end
-
     describe file('/etc/puppetlabs/puppet/device.conf') do
       it { is_expected.to be_file }
-      it { is_expected.to contain %r{[bigip.example.com]} }
-      it { is_expected.to contain %r{type f5} }
-      it { is_expected.to contain %r{[cisco.example.com]} }
-      it { is_expected.to contain %r{type cisco_ios} }
+      it { is_expected.to contain %r{bigip.example.com} }
+      it { is_expected.to contain %r{cisco.example.com} }
     end
-
     describe file('/etc/puppetlabs/puppet/devices/cisco.example.com.conf') do
       it { is_expected.to be_file }
       it { is_expected.to contain %r{address} }
     end
-
     it 'cron for device with run_interval on the proxy agent' do
       result = on(default, 'crontab -l').stdout
-      expect(result).to match(%r{puppet device})
       expect(result).to match(%r{bigip.example.com})
+      expect(result).not_to match(%r{cisco.example.com})
+    end
+  end
+
+  context 'device management via device_manager and device_manager::devices' do
+    it 'define device management in hiera on the master' do
+      define_site_pp(manifest_with_include_devices)
+      define_common_yaml(yaml_with_devices)
+    end
+    it 'define device management on the proxy agent' do
+      run_puppet_agent(allow_changes: true)
+      run_puppet_agent(allow_changes: false)
+    end
+    describe file('/etc/puppetlabs/puppet/device.conf') do
+      it { is_expected.to be_file }
+      it { is_expected.to contain %r{cisco_via_hiera.example.com} }
+    end
+    describe file('/etc/puppetlabs/puppet/devices/cisco_via_hiera.example.com.conf') do
+      it { is_expected.to be_file }
+      it { is_expected.to contain %r{address} }
+    end
+    it 'cron for device with run_interval on the proxy agent' do
+      result = on(default, 'crontab -l').stdout
+      expect(result).to match(%r{cisco_via_hiera.example.com})
+    end
+  end
+
+  context 'undefine device management via device_manager::devices via ensure absent' do
+    it 'undefine device management in hiera on the master' do
+      define_site_pp(manifest_with_include_devices)
+      define_common_yaml(yaml_with_devices_with_ensure_absent)
+    end
+    it 'undefine device management on the proxy agent' do
+      run_puppet_agent(allow_changes: true)
+      run_puppet_agent(allow_changes: false)
+    end
+    describe file('/etc/puppetlabs/puppet/device.conf') do
+      it { is_expected.to be_file }
+      it { is_expected.not_to contain %r{cisco_via_hiera.example.com} }
+    end
+    describe file('/etc/puppetlabs/puppet/devices/cisco_via_hiera.example.com.conf') do
+      it { is_expected.not_to be_file }
+    end
+    it 'undefine cron for device with run_interval on the proxy agent' do
+      result = on(default, 'crontab -l').stdout
+      expect(result).not_to match(%r{cisco_via_hiera.example.com})
     end
   end
 
